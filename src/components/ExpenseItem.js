45c88f2be5bce7d60baeef6d535e4b7db76e09e9
Callum Donaldson <callumdonaldson1990@gmail.com>
2023-01-17 13:51:17 +0000

    added in component for date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
+
   return (
     <div className="expense-item">
-      <div>Date </div>
+     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
     </div>
   );
